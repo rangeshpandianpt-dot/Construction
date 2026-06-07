a22721b885bc7def02b878762c7749821ca1428c
RANGESH <rangeshpandianpt@gmail.com>
2026-06-07 17:41:50 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    tanstackStart({
+      server: {
+        preset: 'vercel',
+      }
+    }),
+    react(),
+    tsconfigPaths(),
+  ],
+});
