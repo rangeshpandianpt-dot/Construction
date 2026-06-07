169d7bb807fdcb6f01eda514bd2ceecaed4aad41
RANGESH <rangeshpandianpt@gmail.com>
2026-06-07 17:48:07 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    tanstackStart({
-      server: {
-        preset: 'vercel',
-      }
+    tanstackStart(),
+    nitro({
+      preset: 'vercel'
     }),
     react(),
     tsconfigPaths(),
